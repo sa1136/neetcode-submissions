e85a7bc60574fba49d88cbadc27b07bb63ae4e5e
Akhilaa Sonduri <72154057+sa1136@users.noreply.github.com>
2026-08-24 14:42:51 -0400

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        int n=nums.size();
+        int far =0;
+
+        for(int i=0;i<n;i++){
+            if(i>far) return false;
+            far = max(far,i+nums[i]);
+            if(far>=n-1) return true;
+
+        }
+
+        return true;
+
+        
+    }
+};
