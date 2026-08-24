dd064c1ba4855015f0310aa54c487fc6e3016dd8
Akhilaa Sonduri <72154057+sa1136@users.noreply.github.com>
2026-08-24 14:51:48 -0400

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        int jump=0;
+        int currend=0;
+        int far=0;
+
+        for(int i=0;i<n-1;i++){
+            far=max(far,i+nums[i]);
+
+            if(i==currend){
+                jump++;
+                currend=far;
+            }
+        }
+        return jump;
+    }
+};
